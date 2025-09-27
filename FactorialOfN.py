5f6c8a86aa6392defee09729510f7ddb2aeaf5ac
Shwetha K <shweetha.sweety17@gmail.com>
2025-09-27 13:01:40 +0530

    25-09-27 :: Recursion : Shwetha K : Factorial of N

--- FactorialOfN.py
+++ FactorialOfN.py
@@ -1,3 +1,19 @@
+'''
+Given the non-negative integers n , compute the factorial of a given number.
+Note: Factorial of n is defined as n * (n -1) * (n - 2) * ... * 1, for n = 0, factorial is 1.
+
+Examples:
+
+Input: n = 5
+Output: 120
+Explanation: 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+Input: n = 4
+Output: 24
+Explanation: 4! = 4 * 3 * 2 * 1 = 24
+
+
+'''
 class Solution:
       def factorialOfN(self,n:int)->int:
           return self.helper(n)
